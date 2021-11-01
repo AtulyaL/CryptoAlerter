4a8b30cbe95f5e097dc29717e5e5c45853fedcaf
AtulyaL <85969346+AtulyaL@users.noreply.github.com>
2021-11-01 08:59:50 -0400

    Add files via upload

---
+++ alertBot.py
@@ -0,0 +1,32 @@
+import requests
+from twilio.rest import Client
+coinMarketCap_Key=""#API KEY
+watchList=["BTC","ETH","DOGE"]
+targets=[45000.0,2500.0,0.23]
+account_sid = ""#Twilio account sid
+auth_token  = ""#Twilio authtoken
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+client = Client(account_sid, auth_token)
+parameters = {
+  'start':'1',
+  'limit':'5000',
+  'convert':'USD'
+}
+head = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': "",#CMC API KEY
+}
+data=requests.get(url,params=parameters,headers=head).json()
+currencies=data['data']
+symbol=[]
+price=[]
+for i in currencies:
+    if(i['symbol'] in watchList):
+        symbol.append(i['symbol'])
+        price.append(i['quote']['USD']['price'])
+for x in range(len(price)):
+    if(price[x]<=targets[x]):
+        message = client.messages.create(
+        to="", #delivery number
+        from_="",#Twilio number
+        body=watchList[x]+" is at "+str(price[x]))
